b3d7ffdc6d8e7e1884fc0aacb8851331f1b45c09
79005 <79005@DESKTOP-O10QFIE>
2021-12-08 13:04:12 +0300

    Добавьте файлы проекта.

---
+++ CharpEx1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharpEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
